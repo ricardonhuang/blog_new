ac32317547907ff33b92ad1d198c8dd07f9b37e2
huangning <huangning@huangning-PC>
2016-12-14 17:13:25 +0800

    add usermodel

--- src/app/models.py
+++ src/app/models.py
@@ -7,6 +7,35 @@ Created on 2016年12月14日
 from flask_sqlalchemy import SQLAlchemy
 from . import db
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    comments = db.relationship('Comment', backref='author', lazy='dynamic')
+    
+    
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
 
 class Post(db.Model):
     __tablename__ = 'posts'
